b623a94f1789575eee6227bf04a76f263b617e41
3sbwya <3sbwya@gmail.com>
2014-02-21 13:27:21 -0500

    Includes javadoc

--- Tower of Hanoi/src/Hanoi/Tower.java
+++ Tower of Hanoi/src/Hanoi/Tower.java
@@ -4,32 +4,57 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 import java.util.Stack;
-
+/**
+ * TOWER OF HAMOI
+ * 
+ * This version of the program is to help figure out how to 
+ * include a javadoc and load everything into a JAR file. 
+ * @author Matt
+ *
+ */
 public class Tower {
 	private Stack<Integer> first = new Stack<>();
 	private Stack<Integer> second = new Stack<>();
 	private Stack<Integer> third = new Stack<>();
 	private int numOfDisks, moves = 0;
 	
+	/**
+	 * Constructor with defined number of disks.
+	 * @param numOfDisks
+	 */
 	public Tower(int numOfDisks) {
 		this.numOfDisks = numOfDisks;
 	}
 	
+	/**
+	 * Default constructor
+	 */
 	public Tower(){
 		this(0);
 	}
 	
+	/**
+	 * Set number of disks.
+	 * @param n
+	 */
 	public void setDisks(int n){
 		this.numOfDisks = n;
 	}
 	
+	/**
+	 * Builds starting peg. Runs the solve method.
+	 * prints number of moves.
+	 */
 	public void play(){
 		buildTower();
 		System.out.println(toString());
 		solve(numOfDisks, first, third, second);
 		System.out.println("Moves: " + moves + "\n");
 	}
-	
+	/**
+	 * Makes sure all stacks are empty. Loads starting
+	 * peg.
+	 */
 	private void buildTower(){
 		first.clear();
 		second.clear();
@@ -60,10 +85,13 @@ public class Tower {
 		return "Peg 1: " + first + "\nPeg 2: " + second + "\nPeg 3:"
 				+ third + "\n";
 	}
-	
+	/**
+	 * Driver
+	 * @param args
+	 * @throws IOException
+	 */
 	public static void main(String[] args) throws IOException{
 		int[] disks = {1,2,3,4,5,6,7,8};
-		boolean check = false;
 		
 		System.out.println("Filename to save moves:");
 		Scanner k = new Scanner(System.in);
